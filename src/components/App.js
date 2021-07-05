321b30bfab30c7a6554d59df20e5638364c87c60
Sharad Maurya <sharadmaurya4600@gmail.com>
2021-07-05 21:49:09 +0530

    First Build

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeList">
+          <li key="relativeListItem1">A</li>
+          <li key="relativeListItem2">B</li>
+          <li key="relativeListItem3">C</li>
+          <li key="relativeListItem4">D</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
